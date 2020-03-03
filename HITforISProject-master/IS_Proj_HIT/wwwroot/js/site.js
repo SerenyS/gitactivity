fe757e05fe9c86bde1ffccc146bdd6fdac2385c4
MJ Hughes <48226678+mj-hughes@users.noreply.github.com>
2020-03-02 20:59:44 -0600

    Create Assessment-update format (#47)
    1) Update pages 2 & 3 to use radio buttons disguised as buttons
    2) Add .js, .css, and html to show informational help modal messages
    3) Slightly compact page 1

--- HITforISProject-master/IS_Proj_HIT/wwwroot/js/site.js
+++ HITforISProject-master/IS_Proj_HIT/wwwroot/js/site.js
@@ -406,15 +406,102 @@ $(function () {
 
     $('.chosen').chosen({ width: '50%' });
 
+    //========= PCA elements ===========
+    // Set Pain values
+    $('#cryingButtonGroup').on('click', 'button', function () {
+        // Get element that was clicked
+        $('#cryingValue').text(this.value);
+    });
+    // PCA information
+    $('#PcaInfoModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var infoSection = button.data('whatever') // Extract info from data-* attributes
+        // Update the modal's content. 
+        // TODO: Is there a way to get this information from the database instead?
+        var msg = ""
+        var ttl = ""
+        if (infoSection.substring(0, 5) == "cries") { ttl = "CRIES Scale" }
+        else if (infoSection.substring(0, 9) == "nonVerbal") { ttl="Non-Verbal Assessment"}
+        if (infoSection == "WongBaker") {
+            ttl = "The FACES Instructions for Usage";
+            msg = 
+            "Explain to the person that each face represents a person who has no pain(hurt), or some, or a lot of pain. Face 0 doesn’t hurt at all.Face 2 hurts just a little bit.Face 4 hurts a little bit more.Face 6 hurts even more.Face 8 hurts a whole lot.Face 10 hurts as much as you can imagine although you don’t have to be crying to have this worst pain.<br /><br />"+
+            "Ask the person to choose the face that best depicts the pain they are experiencing.<br />" +
+                "The FACES Scale Guidelines<br />" +
+                "•	The FACES Scale is recommended for people ages three and older; it is not limited to children.<br/>" +
+                "•	This self - assessment tool must be understood by the patient, so they are able to choose the face that best illustrates the physical pain they are experiencing.The tool is not for use with infants or patients who are unresponsive; other tools have been provided for this purpose<br/>" +
+                "•	It is self - assessment tool and should not be used by a third person, parents, healthcare professionals, or caregivers, to assess the patient’s pain.There are other tools for those purposes.<br/>"
+        } else if (infoSection == "cries") {
+            msg = "CRIES assesses crying, oxygenation, vital signs, facial expression, and sleeplessness. It is often used for infants six months old and younger and is widely used in the neonatal intensive care setting."
+        } else if (infoSection == "criesCrying") {
+            ttl ="Crying - Characteristic cry of pain is high pitched"
+            msg = "0 - No cry or cry that is not high-pitched<br/>" +
+                "1 - Cry high pitched but baby is easily consolable<br/>" +
+                "2 - Cry high pitched and baby is inconsolable<br/>"
+        } else if (infoSection == "criesO2") {
+            ttl = "Requires O2 for SaO2 < 95% - Babies experiencing pain manifest decreased oxygenation. " +
+                "Consider other causes of hypoxemia, e.g., oversedation, atelectasis, pneumothorax)"
+            msg = "0 - No oxygen required<br/>" +
+                "1 - < 30% oxygen required<br/>" +
+                "2 - > 30% oxygen required<br/>"
+        } else if (infoSection == "criesVitalIncrease") {
+            ttl = "Increased vital signs (BP and HR) - Take BP last as this may awaken child making other assessments difficult"
+            msg = "0 - Both HR and BP unchanged or less than baseline<br/>" +
+                "1 - HR or BP increased but increase is < 20% of baseline<br/>" +
+                "2 - HR or BP is increased > 20% over baseline<br/>"
+        } else if (infoSection == "criesExpression") {
+            ttl = "Expression - The facial expression most often associated with pain is a grimace. " +
+                "A grimace may be characterized by brow lowering, eyes squeezed shut, deepening naso-labial furrow, " +
+                "or open lips and mouth."
+            msg = "0 - No grimace present<br/>" +
+                "1 - Grimace alone is present<br/>" +
+                "2 - Grimace and non-cry vocalization grunt it present<br/>"
+        } else if (infoSection == "criesSleepless") {
+            ttl = "Sleepless - Scored based upon the infant's state during the hour preceding this recorded score."
+            msg = "0 - Child has been continuously asleep<br/>" +
+                "1 - Child has awakened at frequent intervals<br/>" +
+                "2 - Child has been awake constantly<br/>"
+        } else if (infoSection == "nonVerbal") {
+            ttl = "Nonverbal Pain Assessment"
+            msg = "Quantifies pain in patients unable to speak (due to intubation, dementia, etc). Each of 5 vitals has up to three fields: vital name, vital value, and vital description."
+        } else if (infoSection == "nonVerbalFace") {
+            ttl = "Face"
+            msg = "0 - No particular expression or smile<br/>" +
+                "1 - Occasional grimace, tearing, frowning, wrinkled forehead<br/>" +
+                "2 - Frequent grimace, tearing, frowning, wrinkled forehead<br/>"
+        } else if (infoSection == "nonVerbalMovement") {
+            ttl = "Activity (movement)"
+            msg = "0 - Lying quietly, normal position<br/>" +
+                "1 - Seeking attention through movement or slow, cautious movement <br /> " +
+                "2 - Restless, excessive activity and/or withdrawal reflexes<br/>"
+        } else if (infoSection == "nonVerbalGuarding") {
+            ttl = "Guarding"
+            msg = "0 - Lying quietly, no positioning of hands over areas of the body<br/>" +
+                "1 - Splinting areas of the body, tense<br/>" +
+                "2 - Rigid, stiff<br/>"
+        } else if (infoSection == "nonVerbalPhysiology") {
+            ttl = "Physiology (vital signs)"
+            msg = "0 - Baseline vital signs unchanged<br/>" +
+                "1 - Change in SBP>20 mmHg or HR>20 bpm<br/>" +
+                "2 - Change in SBP>30 mmHg or HR>25 bpm<br/>"
+        } else if (infoSection == "nonVerbalRespiratory") {
+            ttl = "Respiratory"
+            msg = "0 - Baseline RR/SpO2 synchronous with ventilator<br/>" +
+                "1 - RR>10 bpm over baseline, 5% decrease SpO2 or mild ventilator asynchrony<br/>" +
+                "2 - RR>20 bpm over baseline, 10% decrease SpO2 or severe ventilator asynchrony<br/>"
+        } else { msg = "Not found" }
+        var modal = $(this)
+        modal.find('.modal-title').html(ttl)
+        modal.find('.modal-body p').html(msg)
+    });
+
+
 });
 
 //function to show / hide the textarea within the PCA form
-
 function ShowHide(id, idArea) {
     var yes = document.getElementById(id);
     var area = document.getElementById(idArea);
     area.className = yes.checked ? 'form-control d-block mb-2' : 'd-none';
 }
     
-
-
