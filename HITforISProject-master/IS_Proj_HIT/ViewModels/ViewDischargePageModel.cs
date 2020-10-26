4738ed15eb14794e359050259fbe94dfcf5b86cd
gguttuso <gguttuso@my.wctc.edu>
2020-10-26 14:15:46 -0500

    Discharge screen functionality to display date, disposition and notes.

---
+++ HITforISProject-master/IS_Proj_HIT/ViewModels/ViewDischargePageModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using IS_Proj_HIT.Models;
+
+namespace IS_Proj_HIT.ViewModels
+{
+    public class ViewDischargePageModel
+    {
+        public Encounter Encounter { get; set; }
+        public Patient Patient { get; set; }
+    }
+}
